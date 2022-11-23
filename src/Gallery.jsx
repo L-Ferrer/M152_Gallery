fa0202ccd230570c67b75e408e57c0c6fd325657
L-Ferrer <leandro.ferrer2005@gmail.com>
2022-11-23 15:47:58 +0100

    Changed to lightbox

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,42 +1,36 @@
 import { useState } from 'react'
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
+import Lightroom from 'react-lightbox-gallery'
 import './App.css'
 import cimg1 from './assets/class1.jpg'
 import cimg2 from './assets/class2.jpg'
 import cimg3 from './assets/class3.jpg'
 
-const itemData = [
+const images = [
   {
-    img: cimg1,
-    title: 'Class1',
+    src: "https://images.unsplash.com/photo-1577279549270-b9e297533cdd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80",
+    desc: 'Person wearing shoes',
+    sub: 'Gift Habeshaw'
   },
   {
-    img: cimg2,
-    title: 'Class2',
-  },
-  {
-    img: cimg3,
-    title: 'Class3',
+    src: "https://images.unsplash.com/photo-1577277625082-36df4915ebeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    desc: 'Blonde woman wearing sunglasses smiling at the camera ',
+    sub: 'Dmitriy Frantsev'
   }
 ]
+const settings = {
+  columnCount:{
+    default:5,
+    mobile:3,
+    tab:4
+  },
+  mode: 'dark'
+}
 
 function Gallery() {
   return (
     <div className="App">
       <div>
-      <ImageList sx={{ width: 768, height: 432 }} cols={3} rowHeight={164}>
-        {itemData.map((item) => (
-        <ImageListItem key={item.img}>
-          <img
-            src={`${item.img}?w=768&h=432&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=768&h=432&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
-        </ImageListItem>
-        ))}
-      </ImageList>
+        <Lightroom images={images} settings={settings} />
       </div>
     </div>
   )
