f23c0d79f1718b2bb6aa7182825a059e8377e8da
L-Ferrer <leandro.ferrer2005@gmail.com>
2022-11-24 10:20:21 +0100

    Optimized imports

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,18 +2,18 @@ import Lightroom from 'react-lightbox-gallery'
 import './App.css'
 
 //Images
-import quantum_dog from '../public/18f11103dd833e388c91a9c9ff64642079c8f894e1a6f6d7e2b608984d24a040_1.jpg'
-import society from '../public/277598281_284332320531466_2748313775937039729_n.jpg'
-import DBZ from '../public/pic.png'
-import drone from '../public/Screenshot (20).png'
-import frnch from '../public/Screenshot_20220131-192426_Reddit.jpg'
-import brazil_nr1 from '../public/Screenshot_20220523-235533_Reddit.jpg'
-import patrick from '../public/tckxwcaupyk81.jpg'
-import butterdawg from '../public/vjbq0agkk8q81.jpg'
+import quantum_dog from '/18f11103dd833e388c91a9c9ff64642079c8f894e1a6f6d7e2b608984d24a040_1.jpg'
+import society from '/277598281_284332320531466_2748313775937039729_n.jpg'
+import DBZ from '/pic.png'
+import drone from '/Screenshot (20).png'
+import frnch from '/Screenshot_20220131-192426_Reddit.jpg'
+import brazil_nr1 from '/Screenshot_20220523-235533_Reddit.jpg'
+import patrick from '/tckxwcaupyk81.jpg'
+import butterdawg from '/vjbq0agkk8q81.jpg'
 
 //GIFs
-import burger from '../public/burger-bussin.gif'
-import hacked_phone from '../public/phone_hacked.gif'
+import burger from '/burger-bussin.gif'
+import hacked_phone from '/phone_hacked.gif'
 
 const images = [
   {
