8ea98bc71b74ca6d76a5afe9010478f5d4c402de
L-Ferrer <leandro.ferrer2005@gmail.com>
2023-01-16 10:44:39 +0100

    Added animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ function Home() {
         <>
         <div className="home">
             <h1 className="title">M152 Media App</h1>
+            <img className="gif" src="/animation_crop.gif" align="center"></img>
         </div>
         </>
     )
